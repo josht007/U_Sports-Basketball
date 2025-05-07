867aef116d798dd103fcac3d7e60d03622527962
josht007 <148504666+josht007@users.noreply.github.com.>
2025-05-06 20:57:07 -0400

    Initial commit

---
+++ src/main/java/com/usportsapp/u_sportsapp/USportsApplication.java
@@ -0,0 +1,13 @@
+package com.usportsapp.u_sportsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class USportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(USportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usportsapp/u_sportsapp/USportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usportsapp.u_sportsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class USportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
